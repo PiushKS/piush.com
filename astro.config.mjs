d76426ee17ec169f4e7856dc7ceeb874c006188a
piush <piush.net@gmail.com>
2026-09-01 00:57:52 +0530

    Upgrade to Node 24 and revamp website design aesthetics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://piush.com',
+  site: 'https://piush.com',
   integrations: [mdx(), sitemap()],
-
   vite: {
     plugins: [tailwindcss()],
   },
